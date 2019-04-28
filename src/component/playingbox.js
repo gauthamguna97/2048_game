1a5cc7dc7ca35b5906c7fe60367c71ffce87809d
Gautham G <41054760+gauthamguna97@users.noreply.github.com>
2019-04-28 14:24:35 +0530

    Game development almost done
    all directions handled and few issues left

--- src/component/playingbox.js
+++ src/component/playingbox.js
@@ -33,35 +33,92 @@ export default class Game extends React.Component {
       });
     }
   }
-
-  calculatenewRow(row, i, j, next) {
+  calculatenewRowRight(row, i, next) {
+    if (i !== row.length - 1) {
+      console.log("row", row, i, next);
+      let _row = row;
+      let _next = next;
+      if (row[i + 1] === row[i] && row[i + 1] !== 0 && next) {
+        _row[i + 1] = 2 * row[i];
+        _row[i] = 0;
+        _next = false;
+      } else if (row[i + 1] === 0) {
+        _row[i + 1] = row[i];
+        if (i - 1 >= 0) {
+          for (var j = i; j > 0; j--) {
+            _row[j] = row[j - 1];
+          }
+          _row[0] = 0;
+        } else {
+          _row[i] = 0;
+        }
+      }
+      this.calculatenewRowRight(_row, i + 1, _next);
+    }
+    return row;
+  }
+  calculatenewRow(row, i, next) {
     if (i !== 0) {
       let _row = row;
       let _next = next;
-      if (row[i - 1] === row[j] && row[i - 1] !== 0 && next) {
-        _row[i - 1] = 2 * row[j];
-        _row[j] = 0;
+      if (row[i - 1] === row[i] && row[i - 1] !== 0 && next) {
+        _row[i - 1] = 2 * row[i];
+        _row[i] = 0;
         _next = false;
       } else if (row[i - 1] === 0) {
-        row[i - 1] = row[j];
-        row[j] = 0;
+        _row[i - 1] = row[i];
+        if (i + 1 <= row.length - 1) {
+          for (var j = i; j < row.length - 1; j++) {
+            _row[j] = row[j + 1];
+          }
+          _row[row.length - 1] = 0;
+        } else {
+          _row[i] = 0;
+        }
       }
-      this.calculatenewRow(_row, i - 1, j - 1, _next);
+      this.calculatenewRow(_row, i - 1, _next);
     }
-    console.log(row, i, j);
+    for (var i = 1; i < row.length; i++) {}
     return row;
   }
+
+  transpose(input) {
+    let newArray = [];
+    for (var i = 0; i < input.length; i++) {
+      newArray.push([]);
+    }
+    for (var i = 0; i < input.length; i++) {
+      for (var j = 0; j < input.length; j++) {
+        newArray[j].push(input[i][j]);
+      }
+    }
+    return newArray;
+  }
+
   settable(button) {
     let { tableList } = this.state;
     let newList = tableList;
     console.log(tableList, button);
     if (button === "Left") {
       for (var i = 0; i < tableList.length; i++) {
-        newList[i] = this.calculatenewRow(tableList[i], 2, 2, true);
+        newList[i] = this.calculatenewRow(tableList[i], 2, true);
       }
     } else if (button === "Right") {
+      for (var i = 0; i < tableList.length; i++) {
+        newList[i] = this.calculatenewRowRight(tableList[i], 0, true);
+      }
     } else if (button === "Up") {
+      tableList = this.transpose(tableList);
+      for (var i = 0; i < tableList.length; i++) {
+        newList[i] = this.calculatenewRow(tableList[i], 2, true);
+      }
+      newList = this.transpose(newList);
     } else {
+      tableList = this.transpose(tableList);
+      for (var i = 0; i < tableList.length; i++) {
+        newList[i] = this.calculatenewRowRight(tableList[i], 0, true);
+      }
+      newList = this.transpose(newList);
     }
     newList.forEach((item, index) => {
       item.forEach((_item, _index) => {
